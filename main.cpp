161dd99a29f59d75dc4303fbb2d191f3d5357322
Peter Lucia <peterlucia@gatech.edu>
2021-03-09 23:15:12 -0800

    More updates

--- main.cpp
+++ main.cpp
@@ -53,7 +53,39 @@ public:
     }
 };
 
-int main() {
+int doFib() {
     Fib f;
     cout << f.fib(3);
+}
+
+class FizzBuzz {
+public:
+    vector<string> fizzBuzz(int n) {
+        // multiples of 3 output Fizz
+        // multiples of 5 output Buzz
+        // multiples of both 3 and 5 output Buzz
+        vector<string> result;
+        for (int i = 1; i <= n; ++i)
+        {
+            string s = to_string(i);
+            if (i % 3 == 0) {
+                s = "Fizz";
+            }
+            if (i % 5 == 0) {
+                if (s == "Fizz")
+                    s.append("Buzz");
+                else
+                    s = "Buzz";
+            }
+            result.push_back(s);
+        }
+        return result;
+    }
+};
+
+int main() {
+    FizzBuzz fb;
+    vector<string> result = fb.fizzBuzz(15);
+    for (const string &val : result)
+        cout << val << " ";
 }
