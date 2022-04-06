2fd48b49ec95e54669e9b9e9701318510163c0eb
Peter Lucia <plucia19@gmail.com>
2022-04-05 21:32:20 -0700

    in progress

--- src/time_formatter.h
+++ src/time_formatter.h
@@ -7,12 +7,14 @@
 
 #include <iostream>
 #include <string>
+#include <ctime>
 #include "boost/date_time/posix_time/posix_time.hpp"
 #include "boost/date_time/local_time_adjustor.hpp"
 #include "boost/date_time/c_local_time_adjustor.hpp"
 #include "boost/date_time/local_time/local_time.hpp"
 #include "boost/date_time/time_facet.hpp"
 #include <locale>
+#include <stdio.h>
 
 // std
 using std::string;
@@ -50,41 +52,78 @@ public:
      *      https://www.boost.org/doc/libs/1_78_0/doc/html/date_time/examples.html#date_time.examples.local_utc_conversion
      *      https://en.cppreference.com/w/cpp/chrono/parse
      *      https://stackoverflow.com/questions/26901009/boost-how-to-parse-following-string-to-date-time
+     *      https://www.cplusplus.com/reference/ctime/strftime/
      */
-    static string convert_isoformatted_time(const string &provided_time) {
+    string convert_isoformatted_time(const string &utc_time, string output_offset = "+00:00") {
 
-        // specify desired timezone, hardcoded for now temporarily
-        boost::date_time::local_adjustor<ptime, -8, us_dst> us_california;
+        unsigned int year = 0;
+        unsigned int month = 0;
+        unsigned int day = 0;
+        unsigned int hour = 0;
+        unsigned int minute = 0;
+        float second = 0;
+        int offset_hours = 0;
+        unsigned int offset_minutes = 0;
+        char sign_of_offset = '+';
 
-        // rfc3339 datetime format definition
-        const string fmt = "%Y-%m-%dT%H:%M:%s%Q";
+        string c_fmt = "%04u-%02u-%02uT%02u:%02u:%f%c%02d:%02u";
+        sscanf(utc_time.c_str(), c_fmt.c_str(), &year, &month, &day, &hour, &minute, &second, &sign_of_offset, &offset_hours,
+               &offset_minutes);
 
-        // format the input
-        local_time_input_facet *input_facet = new local_time_input_facet(fmt);
-        input_facet->format(fmt.c_str());
-        const locale input_loc = locale(locale("en_US"), input_facet);
-        stringstream input_ss(provided_time);
-        input_ss.imbue(input_loc);
-        input_ss.str("");
-        input_ss << provided_time;
+        int offset_operator = 1;
+        if (sign_of_offset == '-')
+            offset_operator = -1;
 
-        // add provided time str to input ss
-        ptime utc_time(not_a_date_time);
-        input_ss >> utc_time;
+        long offset_seconds = offset_operator*offset_hours/(60*60)
+                + offset_operator*offset_minutes/60; // gmt off is in seconds.
 
-        // perform timezone adjustment
-        ptime local_time = us_california.utc_to_local(utc_time);
+        std::tm t;
+        t.tm_year = year;
+        t.tm_mon = month;
+        t.tm_mday = day;
+        t.tm_hour = hour;
+        t.tm_min = minute;
+        t.tm_sec = second;
+        t.tm_gmtoff = offset_seconds;
 
-        // format the output
-        local_time_facet* output_facet = new local_time_facet();
-        output_facet->format(fmt.c_str());
-        stringstream output_ss;
+        std::time_t seconds_since_epoch = mktime(&t);
 
-        const locale output_loc = locale(locale("en_US"), output_facet);
-        output_ss.imbue(output_loc);
-        output_ss.str("");
-        output_ss << local_time;
-        return output_ss.str();
+        char sign_of_output_offset = '+';
+        unsigned int output_offset_hours = 0;
+        unsigned int output_offset_minutes = 0;
+
+        sscanf(output_offset.c_str(),
+               "%c%02u:%02u",
+               &sign_of_output_offset,
+               &output_offset_hours,
+               &output_offset_minutes);
+
+        int output_offset_operator = 1;
+        if (sign_of_output_offset == '-')
+            output_offset_operator = -1;
+
+        long output_offset_seconds = output_offset_operator*output_offset_hours/(60*60)
+                + output_offset_operator*output_offset_minutes/60; // gmt off is in seconds.
+
+        seconds_since_epoch += output_offset_seconds;
+
+        auto ymd = date::year_month_day{seconds_since_epoch};
+
+        // https://www.cplusplus.com/reference/cstdio/printf/
+        char output[1024];
+        snprintf(output, sizeof(output), "%04u-%02u-%02uT%02u:%02u:%.6f%c%02u:%02u",
+                 year,
+                 month,
+                 day,
+                 hour,
+                 minute,
+                 second,
+                 offset_char,
+                 (tm_gmtoff_minutes % 24),
+                 ((tm_gmtoff_minutes % 24) % 60)
+                 );
+        return output;
+
     }
 
 };
